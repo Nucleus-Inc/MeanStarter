6549a3740c4961e44755964175db8d91d1c89403
Igor Lopes <igor@nucleus.eti.br>
2018-05-14 15:34:27 -0300

    Change config/express.js to ES6

--- config/express.js
+++ config/express.js
@@ -1,21 +1,21 @@
-var config = require('./config.js')
-var express = require('express')
-var passport = require('passport')
-var flash = require('connect-flash')
-var load = require('express-load')
-var cookieParser = require('cookie-parser')
-var bodyParser = require('body-parser')
-var bodyParserError = require('bodyparser-json-error')
-var session = require('express-session')
-var helmet = require('helmet')
+const config = require('./config.js')
+const express = require('express')
+const passport = require('passport')
+const flash = require('connect-flash')
+const load = require('express-load')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const bodyParserError = require('bodyparser-json-error')
+const session = require('express-session')
+const helmet = require('helmet')
 /* Winston logger */
-var winston = require('winston')
-var expressWinston = require('express-winston')
-var WinstonMongo = require('winston-mongodb').MongoDB
+const winston = require('winston')
+const expressWinston = require('express-winston')
+const WinstonMongo = require('winston-mongodb').MongoDB
 
 module.exports = function () {
   /* Express app */
-  var app = express()
+  const app = express()
   app.set('port', (process.env.PORT || 5000))
 
   /* Ejs views */
