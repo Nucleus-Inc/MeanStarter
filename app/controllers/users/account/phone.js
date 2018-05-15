5757432dd431dbc360bdb5dd175e9ce4889e5525
Igor Lopes <igor@nucleus.eti.br>
2018-05-15 00:30:03 -0300

    Change /users/:id/account/phone-number PATCH/PUT to ES6

--- app/controllers/users/account/phone.js
+++ app/controllers/users/account/phone.js
@@ -1,150 +1,68 @@
-var async = require('async')
+
+const { validationResult } = require('express-validator/check')
 
 module.exports = function (app) {
-  var User = app.models.user
-  var broadcast = app.libs.broadcast
-  var random = app.libs.random
-  var controller = {}
+  const User = app.models.user
+  const random = app.libs.random
+  const controller = {}
 
-  controller.setPhoneChangeCode = function (req, res) {
-    async.waterfall([
-      function (done) {
-        req.checkBody({
-          'phoneNumber': {
-            notEmpty: {
-              errorMessage: 'Phone number is required'
-            },
-            isPhoneNumber: {
-              number: req.body.phoneNumber,
-              errorMessage: 'Invalid phone number'
-            }
-          }
+  controller.setPhoneChangeCode = async (req, res, next) => {
+    try {
+      validationResult(req).throw()
+
+      let code = random.generate(4, 'numeric')
+
+      let user = await User.findById(req.params.id)
+
+      if (!user) {
+        res.status(404).end()
+      } else {
+        let changeRequests = user.changeRequests
+        changeRequests.phoneNumber.newNumber = req.body.phoneNumber
+        changeRequests.phoneNumber.token = new User().generateHash(code.toString())
+        changeRequests.phoneNumber.tokenExp = Date.now() + 300000
+        await User.findByIdAndUpdate(user._id, {
+          changeRequests: changeRequests
         })
-        req.getValidationResult().then(function (result) {
-          if (!result.isEmpty()) {
-            res.status(400)
-            res.json({
-              code: 4000,
-              errors: result.array()
-            })
-          } else {
-            done(null)
-          }
-        })
-      },
-      function (done) {
-        var code = random.generate(4, 'numeric')
-        User.findById(req.params.id).then(function (data) {
-          if (!data) {
-            res.status(404)
-            res.end()
-          } else {
-            var changeRequests = data.changeRequests
-            changeRequests.phoneNumber.newNumber = req.body.phoneNumber
-            changeRequests.phoneNumber.token = new User().generateHash(code.toString())
-            changeRequests.phoneNumber.tokenExp = Date.now() + 300000
-            User.findByIdAndUpdate(data._id, {
-              changeRequests: changeRequests
-            }).then(function (data) {
-              done(null, changeRequests.phoneNumber.newNumber, code)
-              res.end()
-            }).catch(function (err) {
-              res.status(500)
-              res.json({
-                code: 5000,
-                err
-              })
-            })
-          }
-          return null
-        }).catch(function (err) {
-          res.status(500)
-          res.json({
-            code: 5000,
-            err
-          })
-        })
-      },
-      function (recipient, code, done) {
         if (process.env.NODE_ENV !== 'production') {
           res.set('code', code)
         }
-        broadcast.sendSms(recipient, 'Your confirmation code is ' + code)
         res.end()
       }
-    ], function (err, result) {
-      return err || result
-    })
+    } catch (ex) {
+      next(ex)
+    }
   }
 
-  controller.updatePhone = function (req, res) {
-    async.waterfall([
-      function (done) {
-        req.checkParams({
-          'id': {
-            notEmpty: true,
-            isObjectId: {
-              _id: req.params.id
-            }
-          }
+  controller.updatePhone = async (req, res, next) => {
+    try {
+      validationResult(req).throw()
+
+      let user = await User.findById(req.params.id)
+
+      if (!user) {
+        res.status(404).end()
+      } else if (new User().compareHash(req.body.token.toString(), user.changeRequests.phoneNumber.token) &&
+        Date.now() < user.changeRequests.phoneNumber.tokenExp) {
+        var changeRequests = user.changeRequests
+        var newNumber = changeRequests.phoneNumber.newNumber
+        changeRequests.phoneNumber = {}
+        await User.findByIdAndUpdate(user._id, {
+          isActive: true,
+          phoneNumber: newNumber,
+          changeRequests: changeRequests
         })
-        req.checkBody({
-          'token': {
-            notEmpty: true
-          }
-        })
-        req.getValidationResult().then(function (result) {
-          if (!result.isEmpty()) {
-            res.status(400)
-            res.json({
-              code: 4000,
-              errors: result.array()
-            })
-          } else {
-            done(null)
-          }
-        })
-      },
-      function (done) {
-        User.findById(req.params.id).then(function (data) {
-          if (!data) {
-            res.status(404)
-            res.end()
-          } else if (new User().compareHash(req.body.token.toString(), data.changeRequests.phoneNumber.token) &&
-            Date.now() < data.changeRequests.phoneNumber.tokenExp) {
-            var changeRequests = data.changeRequests
-            var newNumber = changeRequests.phoneNumber.newNumber
-            changeRequests.phoneNumber = {}
-            User.findByIdAndUpdate(data._id, {
-              isActive: true,
-              phoneNumber: newNumber,
-              changeRequests: changeRequests
-            }).then(function (data) {
-              res.end()
-            }).catch(function (err) {
-              res.status(500)
-              res.json({
-                code: 5000
-              })
-            })
-          } else {
-            res.status(403)
-            res.json({
-              status: 403,
-              code: 4301
-            })
-          }
-          return null
-        }).catch(function (err) {
-          res.status(500)
-          res.json({
-            code: 5000
-          })
+        res.end()
+      } else {
+        res.status(403)
+        res.json({
+          status: 403,
+          code: 4301
         })
       }
-    ], function (err, result) {
-      return err || result
-    })
+    } catch (ex) {
+      next(ex)
+    }
   }
 
   return controller
