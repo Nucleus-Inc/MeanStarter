8e61c056a6a1816234b2394c1348a15811fb5b77
Igor Lopes <igor@nucleus.eti.br>
2018-05-15 00:51:07 -0300

    change /verifications/password POST to ES6

--- app/controllers/verifications/account/password.js
+++ app/controllers/verifications/account/password.js
@@ -1,39 +1,19 @@
-var async = require('async')
-var zxcvbn = require('zxcvbn')
+const zxcvbn = require('zxcvbn')
+const { validationResult } = require('express-validator/check')
 
-module.exports = function (app) {
-  var controller = {}
+module.exports = (app) => {
+  const controller = {}
 
-  controller.validatePassword = function (req, res) {
-    async.waterfall([
-      function (done) {
-        req.checkBody({
-          'password': {
-            notEmpty: {
-              errorMessage: 'Password is required'
-            }
-          }
-        })
-        req.getValidationResult().then(function (result) {
-          if (!result.isEmpty()) {
-            res.status(400)
-            res.json({
-              code: 4000,
-              errors: result.array()
-            })
-          } else {
-            done(null)
-          }
-        })
-      },
-      function (done) {
-        res.json({
-          score: zxcvbn(req.body.password).score
-        })
-      }
-    ], function (err, result) {
-      return err || result
-    })
+  controller.validatePassword = (req, res, next) => {
+    try {
+      validationResult(req).throw()
+
+      res.json({
+        score: zxcvbn(req.body.password).score
+      })
+    } catch (ex) {
+      next(ex)
+    }
   }
 
   return controller
---
+++ app/routes/verifications/password.js
@@ -0,0 +1,11 @@
+const { check } = require('express-validator/check')
+
+module.exports = (app) => {
+  const controller = app.controllers.verifications.account.password
+
+  app.route('/verifications/password')
+    .post([
+      check('password')
+        .exists()
+    ], controller.validatePassword)
+}
