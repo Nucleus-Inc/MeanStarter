769e0779cd668c7c63c12bd5b14317161c7a4c16
Igor Lopes <igor@nucleus.eti.br>
2018-05-18 16:53:59 -0300

    Update strategies

--- config/strategies/local.js
+++ config/strategies/local.js
@@ -1,13 +1,11 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 
-// Serialize the user
 module.exports = (User) => {
   passport.serializeUser((user, done) => {
     done(null, user._id)
   })
 
-  // Deserialize the user
   passport.deserializeUser((id, done) => {
     User.findById(id, (err, user) => {
       done(err, user)
@@ -15,24 +13,17 @@ module.exports = (User) => {
   })
 
   passport.use('local-login', new LocalStrategy({
-    // By default, local strategy uses username and password, we will override with email
     usernameField: 'email',
     passwordField: 'password',
-    passReqToCallback: true // allows us to pass back the entire request to the callback
+    passReqToCallback: true
   },
   (req, email, password, done) => {
-    // callback with email and password from our form
-    // find a user whose email is the same as the forms email
-    // we are checking to see if the user trying to login already exists
     User.findOne({
       'email': email
     }, (err, user) => {
-      // if there are any errors, return the error before anything else
       if (err) {
         return done(err)
-      } else
-      // if no user is found, return the message
-      if (user && new User().compareHash(password, user.password)) {
+      } else if (user && new User().compareHash(password, user.password)) {
         return done(null, user)
       } else {
         return done(null, false)
