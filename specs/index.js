eab45f77ced0d6ca3930ef075f30b0b4ee872ff1
Igor Lopes <igor@nucleus.eti.br>
2018-05-18 16:56:29 -0300

    Move users specs under /resources

--- specs/index.js
+++ specs/index.js
@@ -2,10 +2,10 @@
 require('./hooks.js')
 
 /* Define specs order */
-require('./users/account/signup.spec.js')
-require('./users/account/signin.spec.js')
-require('./users/account/activation.spec.js')
-require('./users/account/password.spec.js')
-require('./users/account/recovery.spec.js')
-require('./users/account/phone.spec.js')
-require('./users/account/email.spec.js')
+require('./resources/users/account/signup.spec.js')
+require('./resources/users/account/signin.spec.js')
+require('./resources/users/account/activation.spec.js')
+require('./resources/users/account/password.spec.js')
+require('./resources/users/account/recovery.spec.js')
+require('./resources/users/account/phone.spec.js')
+require('./resources/users/account/email.spec.js')
