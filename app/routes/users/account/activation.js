4a09e710f02190b3f4027402efd8b6d55ed8da04
Igor Lopes <igor@nucleus.eti.br>
2018-08-25 16:26:40 -0300

    Fix #16

--- app/routes/users/account/activation.js
+++ app/routes/users/account/activation.js
@@ -1,24 +1,34 @@
+const passportMult = require('app/middlewares/passport-mult.js')
+const users = require('app/middlewares/users.js')
 const { check } = require('express-validator/check')
 
-module.exports = (app) => {
+module.exports = app => {
   const controller = app.controllers.users.account.activation
   const customValidators = app.libs.validators.custom
 
-  app.route('/users/:id/account/activation')
-    .patch([
-      check('id')
-        .exists()
-        .custom((value, { req }) => {
-          return customValidators.isObjectId(req.params.id)
-        })
-    ], controller.setActivationCode)
-    .put([
-      check('id')
-        .exists()
-        .custom((value, { req }) => {
-          return customValidators.isObjectId(req.params.id)
-        }),
-      check('token')
-        .exists()
-    ], controller.activateUser)
+  app
+    .route('/users/:id/account/activation')
+    .patch(
+      [
+        check('id')
+          .exists()
+          .custom((value, { req }) => {
+            return customValidators.isObjectId(req.params.id)
+          })
+      ],
+      controller.setActivationCode
+    )
+    .put(
+      [
+        check('id')
+          .exists()
+          .custom((value, { req }) => {
+            return customValidators.isObjectId(req.params.id)
+          }),
+        check('token').exists()
+      ],
+      passportMult.isAuth,
+      users.verifyOwner,
+      controller.activateUser
+    )
 }
--- app/routes/users/index.js
+++ app/routes/users/index.js
@@ -1,6 +1,10 @@
-module.exports = (app) => {
+const passportMult = require('app/middlewares/passport-mult.js')
+const users = require('app/middlewares/users.js')
+
+module.exports = app => {
   const controller = app.controllers.users.index
 
-  app.route('/users')
-    .get(controller.getUsers)
+  app
+    .route('/users')
+    .get(passportMult.isAuth, users.verifyOwner, controller.getUsers)
 }
