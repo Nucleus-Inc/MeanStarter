660134607249b2ef649efb680fac5aaeab34f2f5
Igor Lopes <igor@nucleus.eti.br>
2018-08-27 21:13:42 -0300

    Add express-mongo-sanitiz

--- app/routes/hello/index.js
+++
@@ -1,7 +0,0 @@
-const { check } = require('express-validator/check')
-
-module.exports = app => {
-  const controller = app.controllers.hello.index
-
-  app.route('/hello').post(controller.getHello)
-}
