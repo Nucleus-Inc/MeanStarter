1e933d8066a6663f7d05ac59125d6e3364102051
Igor Lopes <igor@nucleus.eti.br>
2018-10-01 11:28:43 -0300

    Reorganize strategies

--- config/passport/passport.js
+++ config/passport/passport.js
@@ -1,6 +1,6 @@
 module.exports = app => {
-  require('./strategies/jwt.js')(app)
-  require('./strategies/local.js')(app)
+  require('./strategies/local/jwt.js')(app)
+  require('./strategies/local/local.js')(app)
   require('./strategies/google/signin.js')(app)
   require('./strategies/google/oauth2.js')(app)
   require('./strategies/facebook/oauth2.js')(app)
