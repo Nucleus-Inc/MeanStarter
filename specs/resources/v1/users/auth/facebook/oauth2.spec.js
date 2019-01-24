9443e182eb0cc6543a57036ffdd8ae407396bc5f
Igor Lopes <igor@nucleus.eti.br>
2019-01-24 09:21:50 -0300

    Setup facebook auth specs

--- specs/resources/v1/users/auth/facebook/oauth2.spec.js
+++ specs/resources/v1/users/auth/facebook/oauth2.spec.js
@@ -1,27 +1,27 @@
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const server = require('app.js')
-should = chai.should()
+const should = require('chai')
 
 // let user = require('specs/resources/v1/schemas/user.js')
+
+const fbMockProfile = {
+  id: 'oauth-test',
+  provider: 'facebook-oauth2',
+  displayName: 'John Doe',
+  emails: [{ value: 'john.doe@email.com' }],
+  photos: [
+    {
+      value: 'https://via.placeholder.com/350x150'
+    }
+  ]
+}
 
 chai.use(chaiHttp)
 
 describe('Facebook Authentication', () => {
   before(() => {
     let strategy = server.locals.passport.user._strategies['facebook-oauth2']
-
-    const fbMockProfile = {
-      id: 1234,
-      provider: 'facebook-oauth2',
-      displayName: 'John Doe',
-      emails: [{ value: 'john.doe@email.com' }],
-      photos: [
-        {
-          value: 'https://via.placeholder.com/350x150'
-        }
-      ]
-    }
 
     strategy._passAuthentication = true
     strategy._redirectToCallback = true
@@ -30,12 +30,53 @@ describe('Facebook Authentication', () => {
     strategy._profile = fbMockProfile
   })
 
-  it('should successfully login on /api/v1/users/auth/facebook/oauth2 and create User account GET', done => {
+  it('should successfully login on /api/v1/users/auth/facebook/oauth2 GET', done => {
     chai
       .request(server)
       .get('/users/auth/facebook/oauth2')
       .end((err, res) => {
         res.should.have.status(200)
+        done()
+      })
+  })
+
+  it('should successfully create a new local and facebook account objects with Facebook info  GET', done => {
+    const User = server.models.user
+
+    User.findOne({
+      'account.facebook.id': fbMockProfile.id
+    })
+      .lean()
+      .then(result => {
+        result.should.have.property('_id')
+        result.should.have.property('account')
+        result.account.should.be.a('object')
+        result.account.should.have.property('local')
+        result.account.local.should.be.a('object')
+        result.account.local.should.have.property('email')
+        result.account.local.email.should.be.eql(fbMockProfile.emails[0].value)
+        result.account.local.should.have.property('displayName')
+        result.account.local.displayName.should.be.eql(
+          fbMockProfile.displayName
+        )
+        result.account.local.should.have.property('photo')
+        result.account.local.photo.should.be.eql(fbMockProfile.photos[0].value)
+        result.account.should.have.property('facebook')
+        result.account.facebook.should.be.a('object')
+        result.account.facebook.should.have.property('id')
+        result.account.facebook.id.should.be.eql(fbMockProfile.id)
+        result.account.facebook.should.have.property('email')
+        result.account.facebook.email.should.be.eql(
+          fbMockProfile.emails[0].value
+        )
+        result.account.facebook.should.have.property('displayName')
+        result.account.facebook.displayName.should.be.eql(
+          fbMockProfile.displayName
+        )
+        result.account.facebook.should.have.property('photo')
+        result.account.facebook.photo.should.be.eql(
+          fbMockProfile.photos[0].value
+        )
         done()
       })
   })
